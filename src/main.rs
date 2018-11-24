6d1d3ec00828b8b44e6033f99e7c0a619f747357
Olumide Ogundele <olumideralph@gmail.com>
2018-11-24 15:08:23 +0100

    feat(stage-3): compare user input
    - add loop and error handling mechanism
    -  compare guess value with the secret number

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rand;
 // we need to get input and output data
 // import the io library from the standard library
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng; // using trait to bring the generator of number in scope
 
 
@@ -12,18 +13,37 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is {}", secret_number);
-
     println!("Please input your guess:");
 
-    // variable are immutable by default, so to
-    // make it mutable, you use mut before the variable
-    let mut guess = String::new();
+    loop {
+        // variable are immutable by default, so to
+        // make it mutable, you use mut before the variable
+        let mut guess = String::new();
 
-    // & allowing us to reference a data without copying it
-    // into memory again also using mut to make it mutable
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to readline");
+        // & allowing us to reference a data without copying it
+        // into memory again also using mut to make it mutable
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You need to input an integer");
+                continue;
+            },
+        };
+
+
+        println!("You guessed: {}", guess);
+
+        // do the comparison
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed right!!");
+                break;
+            },
+        }
+    }
 }
